6e340e8bdbc5a957717e17585cad0279b468c687
Shusil <shusilshapkota@gmail.com>
2019-03-08 14:51:07 -0500

    180 Movement both direction
    This code enables robot to move 180 horizontally and vertically

--- SolarTracker/SolarTracker3/SolarTracker3.ino
+++ SolarTracker/SolarTracker3/SolarTracker3.ino
@@ -13,93 +13,36 @@ Servo bottomServo;
 
 int topServoAngle;
 int bottomServoAngle;
+
 int loopCounter;
+
+double diff12;
+double diff13;
+double diff23;
+double precision;
 
 void setup()
 {
 	loopCounter = 0;
-    topServoAngle = 180;
-	
-	bottomServoAngle = 0;
-	topServo.attach(9);
+	precision = 5;
+
+    topServo.attach(9);
 	bottomServo.attach(10);
 
-	topServo.write(180);
-	bottomServo.write(180);
-	delay(1000);
-	topServo.write(69);
-	bottomServo.write(0);
-	delay(1000);
-	topServo.write(180);
-	delay(1000);
+  	topServoAngle = 180;
+	bottomServoAngle = 170;
 
-	Serial.begin(9600);
-}
+	topServo.write(topServoAngle);
+	bottomServo.write(bottomServoAngle);
 
-void loop()
-{
-	updateDiodeValues();
-	
-	loopCounter += 1;
-	if (loopCounter % 60 == 0) {
-		Serial.print(diode1);
-		Serial.print(" "); 
-		Serial.print(diode2); 
-		Serial.print(" "); 
-		Serial.println(diode3); 
-	}
+	delay(2000);
 
-	if (compareDiode(diode2, diode1, 15) == 1)
-	{
-		if(topServoAngle == 69)
-		{
-			if (bottomServoAngle < 90)
-			{
-				bottomServoAngle = 180;
-				bottomServo.write(bottomServoAngle);
-			}
-			else if (bottomServoAngle > 90)
-			{
-				bottomServoAngle = 0;
-				bottomServo.write(bottomServoAngle);
-			}
-			
-			delay(1000);
-		}
-        else if (topServoAngle != 69)
-		{
-            topServoAngle -= 1;
-        }        
-		topServo.write(topServoAngle);
-    }
-	else if (compareDiode(diode2, diode1, 15) == -1)
-	{
-        if (topServoAngle != 180)
-		{
-            topServoAngle += 1;
-        }        
-		topServo.write(topServoAngle);
-    }
-	
-	if (compareDiode(diode1, diode3, 15) == 1)
-	{
-        if (bottomServoAngle != 180)
-		{
-            bottomServoAngle += 1;
-        }        
-		bottomServo.write(bottomServoAngle);
-    }
-	else if (compareDiode(diode1, diode3, 15) == -1)
-	{
-        if (bottomServoAngle != 0)
-		{
-            bottomServoAngle -= 1;
-        }        
-		bottomServo.write(bottomServoAngle);
-    }
-
-	delay(10);
-
+//	topServo.write(69);
+//	bottomServo.write(0);
+//	delay(2000);
+//
+//	topServo.write(180);
+//	delay(2000);
 
 // Servo test sequence
 	// if (topServoAngle > 69.0) {
@@ -110,6 +53,95 @@ void loop()
 	// 	bottomServo.write(0);
 	// 	delay(1000);
 	// }
+
+	Serial.begin(9600);
+}
+
+void loop()
+{
+	updateDiodeValues();
+    updateServoAngles();
+	
+	// loopCounter += 1;
+
+	// if (loopCounter % 1 == 0) {
+	// 	Serial.print(diode1);
+	// 	Serial.print(" "); 
+	// 	Serial.print(diode2); 
+	// 	Serial.print(" "); 
+	// 	Serial.print(diode3); 
+	// 	Serial.println(" ");
+	// 	Serial.print(topServo.read());
+	// 	Serial.println(" ");
+	// 	Serial.print(bottomServo.read());
+	// }
+	
+	diff12 = diode1 - diode2;
+	diff13 = diode1 - diode3;
+	diff23 = diode2 - diode3;
+
+	if (abs(diff13) < precision && abs(diff23) < precision)
+	{
+		topServo.write(topServoAngle);
+	}
+	else if (diff13 > precision || diff23 > precision)
+	{
+		if(topServoAngle == 69)
+		{
+			if (bottomServoAngle < 90)
+			{
+				bottomServoAngle = 170;
+				bottomServo.write(bottomServoAngle);
+			}
+			else if (bottomServoAngle > 90)
+			{
+				bottomServoAngle = 10;
+				bottomServo.write(bottomServoAngle);
+			}
+			
+			delay(1000);
+		}
+		else if (topServoAngle > 69)
+		{
+			topServoAngle -= 1;
+			topServo.write(topServoAngle);	
+		}        
+	}
+	else if (diff13 < -precision || diff23 < -precision)
+	{
+		if (topServoAngle < 180)
+		{
+			topServoAngle += 1;
+			topServo.write(topServoAngle);
+		}        
+	}
+	
+    // TURN //
+
+    updateServoAngles();
+
+	if (abs(diff12) < precision)
+	{
+		bottomServo.write(bottomServoAngle);
+	}
+	else if (diff12 > precision) 
+	{
+		if(bottomServoAngle < 180)
+		{
+			bottomServoAngle += 1;
+   		    bottomServo.write(bottomServoAngle);  
+		}
+	}
+	else if (diff12 < -precision)
+	{
+		if (bottomServoAngle > 0)
+		{
+			bottomServoAngle -= 1;
+			bottomServo.write(bottomServoAngle);
+		}
+	}
+  
+	delay(25);
 }
 
 void updateDiodeValues()
@@ -119,18 +151,39 @@ void updateDiodeValues()
 	diode3 = analogRead(DIODE3);
 }
 
-int compareDiode(double firstDiode, double secondDiode, double precision)
-{ 
-	if (abs(firstDiode - secondDiode) < precision) 
-	{
-		return 0;
-	} 
-	else if (firstDiode > secondDiode)
-	{
-		return 1;
-	}
-	else if (firstDiode < secondDiode)
-	{
-		return -1;
-	}
+void updateServoAngles()
+{
+    topServoAngle = topServo.read();
+    bottomServoAngle = bottomServo.read();
 }
+// int compareDiodeTheta(double precision)
+// { 
+// 	if (abs(diode1 - diode2) < precision) 
+// 	{
+// 		return 0;
+// 	} 
+// 	else if (diode1 - diode2 > precision)
+// 	{
+// 		return 1;
+// 	}
+// 	else if (diode2 - diode1 > precision)
+// 	{
+// 		return -1;
+// 	}
+// }
+
+// int compareDiodePhi(double precision)
+// {
+// 	if (abs(diode2 - diode3) < precision) 
+// 	{
+// 		return 0;
+// 	} 
+// 	else if (diode2 - diode3 > precision)
+// 	{
+// 		return 1;
+// 	}
+// 	else if (diode3 - diode2 > precision)
+// 	{
+// 		return -1;
+// 	}
+// }
